ba7f2b5ebf606c9e00d538278a3316689bfc51af
Peter Omollo <omollpeter@gmail.com>
2024-09-09 16:32:40 +0300

    Write a function that rotates a matrix

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,16 @@
+#!/usr/bin/pythion3
+"""
+Contains function that transposes a 2D matrix
+"""
+
+
+def rotate_2d_matrix(matrix):
+    """
+    Transposes and displays a 2D n*n matrix
+    """
+    for i in range(len(matrix)):
+        for j in range(i + 1, len(matrix[i])):
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+
+    for i in range(len(matrix)):
+        matrix[i].reverse()
---
+++ 0x07-rotate_2d_matrix/README.md
@@ -0,0 +1,1 @@
+### This folder contains a script for rotating a 2D matrix in python
