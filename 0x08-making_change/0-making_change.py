309d891c9998bba294a30e9d45e759d71766ba43
Peter Omollo <omollpeter@gmail.com>
2024-09-18 07:50:55 +0300

    create a function that calculates fewest number of coins needed to make a change

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Contains a function that calculates fewest number of coins needed
+to make a change
+"""
+
+
+def makeChange(coins, total):
+    """
+    Returns fewest number of coins needed to make a change
+    """
+    if type(coins) is not list:
+        return -1
+    total_coins = 0
+
+    coins.sort(reverse=True)
+    while coins:
+        if coins[0] > total:
+            coins.pop(0)
+            continue
+        total_coins += total // coins[0]
+        total %= coins[0]
+
+    if total:
+        return -1
+    return total_coins
---
+++ 0x08-making_change/test.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+total = 25
+
+total %= 5
+
+print(total)
