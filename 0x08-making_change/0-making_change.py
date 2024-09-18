21af5ee1d95fbc0677b99306dcf29f135c377952
Peter Omollo <omollpeter@gmail.com>
2024-09-18 08:05:53 +0300

    Fix making change function

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -9,6 +9,8 @@ def makeChange(coins, total):
     """
     Returns fewest number of coins needed to make a change
     """
+    if total <= 0:
+        return 0
     if type(coins) is not list:
         return -1
     total_coins = 0
